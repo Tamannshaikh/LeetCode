1e947403633f6e83cca9c78713e1b8dcfe2d70b7
Tamannshaikh <136705521+Tamannshaikh@users.noreply.github.com>
2026-08-20 17:10:34 +0530

    Time: 472 ms (74.08%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 select customer_number 
     from Orders
-    group by customer_number 
-    order by count(*) desc
-    limit 1 ;
+    group by customer_number -- group karo customer numbers ko
+    order by count(*) desc   -- count karo ek customer 
+                               -- number kitne bar aya
+    limit 1 ; -- top 1st ko slect karo
