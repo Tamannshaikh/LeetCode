3334637d087dd9ac10d2ffc994d19edb4439707a
Tamannshaikh <136705521+Tamannshaikh@users.noreply.github.com>
2026-08-20 22:49:47 +0530

    Time: 322 ms (45.52%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+-- Triangle Inequality Rules or Property
+-- where l+𝑏>h , l+h>b, h+b>l can make a triangle
+-- if (l + b = h) then Fails to make a triangle
+
+select x,y,z, 
+case
+when x+y>z and x+z>y and y+z>x
+then 'Yes'
+else 'No'
+end as triangle
+from Triangle;
