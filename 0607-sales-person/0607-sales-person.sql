e388678b050b5d0cc92802884a09048e5c0af6d6
Tamannshaikh <136705521+Tamannshaikh@users.noreply.github.com>
2026-08-20 17:55:36 +0530

    Time: 1638 ms (40.6%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -5,5 +5,5 @@ where s.sales_id not in
     (select o.sales_id 
     from Orders o inner join Company c
     on o.com_id = c.com_id
-    where c.name='RED'
+    where c.name='RED' -- why we need to write RED why not 1
 );
