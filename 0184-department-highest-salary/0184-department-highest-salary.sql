c336f63917fa5b57bc56412f3392cc24f61d693f
Tamannshaikh <136705521+Tamannshaikh@users.noreply.github.com>
2026-08-21 14:09:29 +0530

    Time: 1536 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
-select 
-d.name as Department, 
-e.name as Employee,
-e.salary as Salary
-from Employee e inner join Department d
-on d.id = e.departmentId
-where e.salary = (select max(e.salary) from Employee e where d.id = e.departmentId );
+select d.name as Department,
+       e.name as Employee,
+       e.salary as Salary
+from  Employee e inner join Department d
+on e.departmentId=d.id -- only join to get department name
+where e.salary = (   -- hold max salary and keep compare with every one of same dpt
+    select max(e2.salary) -- find max salary from one dept -- e self join with e2
+    From Employee e2
+    where e2.departmentId = e.departmentId  -- slect all emp from same departmnet
+);
+
+
