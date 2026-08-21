e89172db0e2c56690373e70d4cbe7eb96f815c81
Tamannshaikh <136705521+Tamannshaikh@users.noreply.github.com>
2026-08-21 15:21:43 +0530

    Time: 495 ms (76.67%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-select player_id , min(event_date) as first_login
+select player_id, min(event_date) as first_login
 from Activity 
 group by player_id;
+
+-- automaticall take fisrt login in min funcation
+-- group by clause will take only one time player_id one each player to group
