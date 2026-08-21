c4adbb5c9eab679377472b1477ffab0e548b4f2a
Tamannshaikh <136705521+Tamannshaikh@users.noreply.github.com>
2026-08-21 15:11:47 +0530

    Time: 529 ms (47%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
-select w.id from Weather w inner join Weather w1
-on datediff(w.recordDate, w1.recordDate) = 1 
-where w.temperature > w1.temperature;
+select w.id 
+from Weather w inner join Weather w1
+on datediff(w.recordDate,w1.recordDate)=1
+where w.temperature>w1.temperature;
 
 -- datediff() this funcation is use to kept difference between date 
 -- = 1 mean 1 day difference
